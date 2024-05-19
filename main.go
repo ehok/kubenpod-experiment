267ac06dc267283f26f29399fb3d8c5519b90289
Engin Can Höke <engincanhoke@gmail.com>
2024-05-19 17:19:40 +0300

    eighth commit

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "version" {
 		v := version.Get()
-		fmt.Printf("Version: %s\nGit commit: %s\nBuild date: %s\n", v.GitVersion, v.GitCommit, v.BuildDate)
 		fmt.Printf("Version: %s\n", v.GitVersion)
 		fmt.Printf("Git commit: %s\n", v.GitCommit)
 		fmt.Printf("Build date: %s\n", v.BuildDate)
